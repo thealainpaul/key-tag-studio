9ba3355743860333a3ca0b5941a468eaf6fc613e
Alain Paul <131689456+thealainpaul@users.noreply.github.com>
2026-07-27 21:58:24 +0200

    Update DesignerApp.tsx

--- src/components/DesignerApp.tsx
+++ src/components/DesignerApp.tsx
@@ -386,8 +386,8 @@ export default function DesignerApp() {
           </button>
         </div>
 
-        <div style={{ display: "flex", gap: "8px", alignItems: "flex-start", width: "100%", marginBottom: "0.5rem" }}>
-          <input type="checkbox" checked={qrEnabled} onChange={(e) => setQrEnabled(e.target.checked)} style={{ marginTop: "4px", flexShrink: 0, marginLeft: "0" }} />
+        <div style={{ display: "flex", gap: "8px", alignItems: "flex-start", width: "100%" }}>
+          <input type="checkbox" checked={qrEnabled} onChange={(e) => setQrEnabled(e.target.checked)} style={{ marginTop: "2px", flexShrink: 0 }} />
           <span style={{ fontWeight: "bold", fontSize: "14px", minWidth: 0, flex: 1 }}>Check this box to add a QR code and then add your website to the box that appears, for a scannable QR Code to your website.</span>
         </div>
         {qrEnabled && (
